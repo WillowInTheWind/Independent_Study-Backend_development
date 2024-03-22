b60fbe7ca34dd0d5e5a73a06d3d1762a66b272af
WillowintheWind <wayland.chase@gmail.com>
2024-03-21 23:30:58 -0500

    Rust app!

--- rustapi/src/handlers/user_manager.rs
+++ rustapi/src/handlers/user_manager.rs
@@ -2,11 +2,10 @@ use axum::http::StatusCode;
 use axum::Json;
 use sqlx::{Pool, Sqlite};
 use crate::handlers::GenericUser;
-use crate::errors;
 use crate::errors::QueryErrors;
 
 pub(crate) trait UserService: Send + Sync {
-    fn get_users(&self) -> Result<Vec<Json<GenericUser>>, (StatusCode, QueryErrors)>;
+    fn get_users(&self) -> Result<Vec<Json<GenericUser>>, sqlx::Error>;
     fn get_user_by_id(&self) -> Result<Json<GenericUser>, (StatusCode, String)>;
     fn get_user_by_name(&self, name:&str) -> Result<Json<GenericUser>, (StatusCode, String)>;
     fn create_user(&self) -> Result<Json<GenericUser>, (StatusCode, String)>;
@@ -14,11 +13,10 @@ pub(crate) trait UserService: Send + Sync {
     fn edit_username(&self) ->  Result<Json<GenericUser>, (StatusCode, String)>;
 }
 impl UserService for Pool<Sqlite> {
-    async fn get_users(&self) -> Result<Vec<Json<GenericUser>>, (StatusCode, QueryErrors)> {
-        sqlx::query_scalar(todo!())
+    async fn get_users(&self) -> Result<Vec<Json<GenericUser>>, sqlx::Error> {
+        sqlx::query(todo!())
             .fetch_all(&self)
             .await
-            .map_err("query fail".to_string())
     }
 
     fn get_user_by_id(&self) -> Result<Json<GenericUser>, (StatusCode, String)> {
