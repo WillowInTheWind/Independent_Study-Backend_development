ada78606ae6802b5773accbb06b07db86c97fbd2
WillowInTheWind <123593045+WillowInTheWind@users.noreply.github.com>
2024-03-26 17:56:03 -0500

    Oauth implementation

--- rustapi/src/handlers/login_service.rs
+++ rustapi/src/handlers/login_service.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
-use oauth2::{basic::BasicClient, AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken, PkceCodeChallenge, PkceCodeVerifier, RedirectUrl, RevocationUrl, Scope, TokenResponse, TokenUrl};
+use oauth2::{basic::BasicClient, reqwest::async_http_client, AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken, PkceCodeChallenge, PkceCodeVerifier, RedirectUrl, RevocationUrl, Scope, TokenResponse, TokenUrl};
 use anyhow::{Context, Result};
 use async_session::chrono::Utc;
 use axum::extract::{Host, Query, State};
-use axum::response::{IntoResponse, Redirect};
+use axum::response::{IntoResponse, Redirect, Response};
 use oauth2::reqwest::http_client;
 use oauth2::url::Url;
 use uuid::Uuid;
@@ -12,6 +12,10 @@ use crate::types;
 use crate::types::{GenericUser, OauthError};
 use reqwest::get;
 use std::env;
+use async_session::Session;
+use axum::Extension;
+use axum::http::StatusCode;
+use serde::Deserialize;
 use crate::handlers::user_manager::UserService;
 
 pub(crate) fn oauth_client() -> Result<BasicClient, crate::types::OauthError> {
@@ -44,139 +48,98 @@ pub(crate) fn oauth_client() -> Result<BasicClient, crate::types::OauthError> {
 }
 
 
-pub(crate) async fn login(State(state): State<AppState>) -> std::result::Result<Redirect, OauthError> {
+pub(crate) async fn login(Extension(user_data): Extension<Option<GenericUser>>, State(state): State<AppState>, Query(mut params): Query<HashMap<String, String>>, ) -> Redirect {
     // if user_data.is_some() {
     //     // check if already authenticated
     //     return Ok(Redirect::to("/"));
     // // }
-    // let return_url = params
-    //     .remove("return_url")
-    //     .unwrap_or_else(|| "/".to_string());
-
-    // let (pkce_code_challenge, pkce_code_verifier) = PkceCodeChallenge::new_random_sha256();
-    let client = oauth_client()?;
-
-    let (auth_url, csrf_state) = client
+    let (auth_url, _csrf_token) = state.oauth_client.clone()
         .authorize_url(CsrfToken::new_random)
-        .add_scope(Scope::new(
-            "https://www.googleapis.com/auth/userinfo.email".to_string(),
-        ))
-        // .set_pkce_challenge(pkce_code_challenge)
+        .add_scope(Scope::new("profile".to_string()))
+        .add_scope(Scope::new("email".to_string()))
         .url();
-println!("{:?}", client);
-    //  sqlx::query(
-    //     "INSERT INTO oauth2_state_storage (csrf_state, pkce_code_verifier, return_url) VALUES (?, ?, ?);",
-    // )
-    //     .bind(csrf_state.secret())
-    //     .bind(pkce_code_verifier.secret())
-    //     .bind(return_url)
-    //     .execute(&state.dbreference)
-    //     .await?;
-    Ok( Redirect::to(auth_url.as_ref()))
+
+//     // Redirect to Google's oauth service
+// // println!("{:?}", client);
+//      sqlx::query(
+//         "INSERT INTO oauth2_state_storage (csrf_state, pkce_code_verifier, return_url) VALUES (?, ?, ?);",
+//     )
+//         .bind(_csrf_token.secret())
+//         .bind(pkce_code_verifier.secret())
+//         .bind(return_url)
+//         .execute(&state.dbreference)
+//         .await?;
+    Redirect::to(&auth_url.to_string())
     // Redirect to Google's oauth service
 }
 
 pub async fn oauth_return(
-    Query(mut params): Query<HashMap<String, String>>,
+    Query(query): Query<AuthRequest>,
     State(state): State<AppState>,
     Host(hostname): Host,
-) -> Result<impl IntoResponse, OauthError> {
-    let csrf_token = CsrfToken::new(params.remove("state").ok_or("OAuth: without state")?);
-    let code = AuthorizationCode::new(params.remove("code").ok_or("OAuth: without code")?);
-
-    let query: (String, String) = sqlx::query_as(
-        r#"DELETE FROM oauth2_state_storage WHERE csrf_state = ? RETURNING pkce_code_verifier,return_url"#,
-    )
-        .bind(csrf_token.secret())
-        .fetch_one(&state.dbreference)
-        .await?;
-
-
-    let pkce_code_verifier = query.0;
-    let return_url = query.1;
-    let pkce_code_verifier = PkceCodeVerifier::new(pkce_code_verifier);
-
-    // Exchange the code with a token.
-    let client = state.oauth_client.clone();
-    let token_response = tokio::task::spawn_blocking(move || {
-        client
-            .exchange_code(code)
-            .set_pkce_verifier(pkce_code_verifier)
-            .request(http_client)
-    })
+) -> Result<impl IntoResponse, ReturnError> {
+    // Get an auth token
+    let token = state.oauth_client
+        .exchange_code(AuthorizationCode::new(query.code.clone()))
+        .request_async(async_http_client)
         .await
-        .map_err(|_| "OAuth: exchange_code failure")?
-        .map_err(|_| "OAuth: tokio spawn blocking failure")?;
-    let access_token = token_response.access_token().secret();
+        .context("failed in sending request request to authorization server")?;
+        // Fetch user data from discord
+    let client = reqwest::Client::new();
+    let user_data: GenericUser = client
+            // https://discord.com/developers/docs/resources/user#get-current-user
+            .get("https://www.googleapis.com/oauth2/v3/userinfo")
+            .bearer_auth(token.access_token().secret())
+            .send()
+            .await
+            .context("failed in sending request to target Url")?
+            .json::<GenericUser>()
+            .await
+            .context("failed to deserialize response as JSON")?;
 
-    // Get user info from Google
-    let url =
-        "https://www.googleapis.com/oauth2/v2/userinfo?oauth_token=".to_owned() + access_token;
-    let body = get(url)
-        .await
-        .map_err(|_| "OAuth: reqwest failed to query userinfo")?
-        .text()
-        .await
-        .map_err(|_| "OAuth: reqwest received invalid userinfo")?;
-    let mut body: serde_json::Value =
-        serde_json::from_str(body.as_str()).map_err(|_| "OAuth: Serde failed to parse userinfo")?;
-    let email = body["email"]
-        .take()
-        .as_str()
-        .ok_or("OAuth: Serde failed to parse email address")?
-        .to_owned();
-    let verified_email = body["verified_email"]
-        .take()
-        .as_bool()
-        .ok_or("OAuth: Serde failed to parse verified_email")?;
-    if !verified_email {
-        return Err(OauthError::new("OAuth: email address is not verified".to_owned())
-            .with_user_message("Your email address is not verified. Please verify your email address with Google and try again.".to_owned()));
+        // Create a new session filled with user data
+        let mut session = Session::new();
+        session
+            .insert("user", &user_data)
+            .context("failed in inserting serialized value into session")?;
+
+        // Store session and get corresponding cookie
+        //TODO: store session in DB
+
+
+        // Build the cookie
+        // Set cookie
+
+
+        Ok( Redirect::to("/"))
     }
 
-    // Check if user exists in database
-    // If not, create a new user
-    let query: Result<(i64,), _> =
-        sqlx::query_as(r#"SELECT id FROM user WHERE user_email = ?"#)
-            .bind(email.as_str())
-        .fetch_one(&state.dbreference)
-        .await;
-    let user_id = if let Ok(query) = query {
-        query.0
-    } else {
-        let user = GenericUser {
-            id: Some(0),
-            user_name: "".to_string(),
-            user_identifier: 0,
-            user_email: "".to_string(),
-        };
-        let query = state.dbreference.create_user(user).await?;
-        query
-    };
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+pub(crate) struct AuthRequest {
+    code: String,
+    state: String,
+}
 
-    // Create a session for the user
-    let session_token_p1 = Uuid::new_v4().to_string();
-    let session_token_p2 = Uuid::new_v4().to_string();
-    let session_token = [session_token_p1.as_str(), "_", session_token_p2.as_str()].concat();
-    let headers = axum::response::AppendHeaders([(
-        axum::http::header::SET_COOKIE,
-        "session_token=".to_owned()
-            + &*session_token
-            + "; path=/; httponly; secure; samesite=strict",
-    )]);
-    let now = Utc::now().timestamp();
+#[derive(Debug)]
+pub(crate) struct ReturnError(anyhow::Error);
 
-    // sqlx::query(
-    //     "INSERT INTO user_sessions
-    //     (session_token_p1, session_token_p2, user_id, created_at, expires_at)
-    //     VALUES (?, ?, ?, ?, ?);",
-    //     .bind(session_token_p1)
-    //     .bind(session_token_p2)
-    //     .bind(user_id)
-    //     .bind(now)
-    //     .bind(now + 60 * 60 * 24 * 7)
-    //     .execute(&state.dbreference)
-    //     .await?;
+// Tell axum how to convert `AppError` into a response.
+impl IntoResponse for ReturnError {
+    fn into_response(self) -> Response {
+        tracing::error!("Application error: {:#}", self.0);
 
-    Ok((headers, Redirect::to(return_url.as_str())))
+        (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong").into_response()
+    }
+}
+
+// This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
+// `Result<_, AppError>`. That way you don't need to do that manually.
+impl<E> From<E> for ReturnError
+    where
+        E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
 }
