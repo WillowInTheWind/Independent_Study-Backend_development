9008353328b1e1ecd5dd4ba4d0b594339535fc09
WillowInTheWind <123593045+WillowInTheWind@users.noreply.github.com>
2024-04-09 16:06:20 -0500

    date picker

--- Mx-Management-Site/src/app/homepage/homepage.component.ts
+++ Mx-Management-Site/src/app/homepage/homepage.component.ts
@@ -53,7 +53,7 @@ export class HomepageComponent {
   }
   myFilter = (d: Date | null): boolean => {
     const day = (d || new Date()).getDay();
-    // Prevent Saturday and Sunday from being selected.
+
     return day == 1 || day == 5;
   };
 }
